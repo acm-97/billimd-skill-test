cee9e387bf47c2a6494efe23a57af5e30dc76f6d
acm-97 <alejandro97.acm@gmail.com>
2025-03-18 23:22:14 -0400

    [update] duration of animations

--- src/components/dashboard.tsx/cards.tsx
+++ src/components/dashboard.tsx/cards.tsx
@@ -38,7 +38,7 @@ export function CryptoCards({
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        <Fade cascade>
+        <Fade cascade duration={500}>
           {data.map(crypto => (
             <Card
               key={crypto.id}
--- src/components/users/dashboard-stats.tsx
+++ src/components/users/dashboard-stats.tsx
@@ -5,7 +5,7 @@ import {Fade} from 'react-awesome-reveal'
 export default function DashboardStats() {
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-      <Fade cascade>
+      <Fade cascade duration={500}>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between pb-2">
             <CardTitle className="text-sm font-medium">Total Users</CardTitle>
